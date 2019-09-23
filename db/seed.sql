9e4dc8cc691120cb6dca087180b4305372bcb157
nikkisowell <nicolesowell.dev@outlook.com>
2019-09-22 21:35:33 -0500

    initial folders created

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+-- Insert a set of records.
+INSERT INTO burgers (burger_name) VALUES ('Cheesburger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseurger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name,devoured) VALUES ('Dollar Burger',1);
